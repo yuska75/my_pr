063529cf0e2419f2b072716f08b9ae97cc777742
yuska75 <120448743+yuska75@users.noreply.github.com>
2023-03-20 16:33:28 +0300

    add files

---
+++ script.js
@@ -0,0 +1,57 @@
+'use strict';
+
+const numList = document.querySelector('.whod');
+const btnStart = document.querySelector('.start');
+const result = document.querySelector('.first__result')
+const btnCopy = document.querySelector('.copy');
+
+btnStart.addEventListener('click', () => {
+  let num = +numList.value;
+
+function calkNum(num) {
+  let resultOne = [];
+  for (let i = 0; i < num; i++) {
+    if (i % 2 !== 0) {
+      resultOne.push(i)
+    }
+  }
+  return resultOne;
+}
+
+
+let resultOne = calkNum(num);
+
+function calkList(num) {
+  num.splice(0, 2, '1 - 3');
+  return num.join(',');
+}
+
+ let res = calkList(resultOne)
+result.textContent = res;
+});
+
+
+
+
+// let num = 48;
+
+// function calkNum(num) {
+//   let resultOne = [];
+//   for (let i = 0; i < num; i++) {
+//     if (i % 2 !== 0) {
+//       resultOne.push(i)
+//     }
+//   }
+//   return resultOne;
+// }
+
+
+// let resultOne = calkNum(num);
+
+// function calkList(num) {
+//   num.splice(0, 2, '1 - 3');
+//   return num.join(',');
+// }
+
+// console.log(calkList(resultOne));
+
